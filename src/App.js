5dbaaef7bcf6fbec47ba47c34fd99f4fd42fc069
David Probst <probst.dj2@gmail.com>
2018-08-04 09:46:33 -0500

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Project from './components/Project';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Content will go here.</h1>
+        <aside>
+          <nav>
+            <Link to ='/about' className="link">About</Link>
+            <Link to ='/' className="link">Projects</Link>
+            <Link to ='/resume' className="link">Resume</Link>
+            <Link to ='/contact' className="link">Contact</Link>
+          </nav>
+        </aside>
       </div>
     );
   }
