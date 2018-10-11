c219d3a183c5d5e206fa3a2a897bcad067cbeac4
David Probst <probst.dj2@gmail.com>
2018-10-11 14:55:23 -0500

    beginning of project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import projectData from './../data/projects';
 
 class Project extends Component {
+  constructor(props) {
+    super(props);
+
+    const project = project.find( project => {
+      return project.slug === this.props.match.params.slug
+    });
+
+    this.state = { project: project };
+
+  }
+
   render() {
     return (
       <h1>This is the Project component.</h1>
+      <p>The project title is {this.state.project.title}</p>
     );
   }
 }
