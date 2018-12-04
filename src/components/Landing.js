68574587996ef340f72d33228be7eba17aa8c9d5
David Probst <probst.dj2@gmail.com>
2018-12-03 19:57:53 -0600

    home page responsive css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -11,23 +11,23 @@ class Landing extends Component {
   render() {
     return (
       <section id="home">
-        <div id='home-image'>
-          <img src='/assets/images/home-image.jpeg'></img>
-        </div>
         {
         <div className='content'>
           <div id='intro' className="fixed-width">
-            <h1><span className='white-text-background'>David Probst is a digital product designer currently working in Chicago, Illinois.</span></h1>
-            <h3><span className='white-text-background'>Working as the connective element between product vision and product development, he has had the opportunity to work with a range of companies - from Fortune 500 corporations to early stage start ups.</span></h3>
-            <h3><span className='white-text-background'>Today, David is working with DAIS Technology and a Bay Area start up to create modern designs that excite users, and accelerate product development.</span></h3>
-            <Link to='/about' class='about-link'>Read full bio ></Link>
+            <h1 id='title' className='xs-text-padding'><span className='white-text-background'>David Probst is a digital product designer currently working in Chicago, Illinois.</span></h1>
+            <div id='home-image'>
+              <img src='/assets/images/home-image.jpeg'></img>
+            </div>
+            <h3 className='xs-text-padding'><span className='white-text-background'>Working as the connective element between product vision and product development, he has had the opportunity to work with a range of companies - from Fortune 500 corporations to early stage start ups.</span></h3>
+            <h3 className='xs-text-padding'><span className='white-text-background'>Today, David is working with DAIS Technology and a Bay Area start up to create modern designs that excite users, and accelerate product development.</span></h3>
+            <Link to='/about' class='about-link xs-text-padding'>Read full bio ></Link>
           </div>
 
           <div id='recent-projects'>
             {
               this.state.projects.map( (project,index) =>
               <div className='project-preview' key={index}>
-                <div className='project-description'>
+                <div className='project-description xs-text-padding'>
                   <h2>{project.title}</h2>
                   <li className='project-skills-list'>
                     {project.skills.map( (skill,index) =>
